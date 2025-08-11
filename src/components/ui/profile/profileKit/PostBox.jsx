86e7d8144f02867ba306b164788aad1d41d17df2
shakhawat.dev <shakhawat.code@gmail.com>
2025-08-11 10:49:52 +0600

    update status

--- src/components/ui/profile/profileKit/PostBox.jsx
+++ src/components/ui/profile/profileKit/PostBox.jsx
@@ -194,7 +194,7 @@ export default function PostBox({className = "relative" , open , closeFunction ,
              {isEdit.status ?
             <button onClick={()=>editPost(isEdit.id , title , text , featuredImage , category , prevImage , setUpdateStatus , dispatch  )} style={{background:  (title || text) && featuredImage ? "#4B6BFB" : color.bgprimary , color: (title || text) && featuredImage ? color.switchtext : color.textprimary}} className=' md:py-2  px-5 rounded-[6px] cursor-pointer  text-white font-work-sans font-medium text-base  md:leading-6 flex items-center gap-2   '  type="button">
               <span>Update</span>
-              {postingstatus && <TbLoaderQuarter className='text-xl animate-rotate  '/>}
+              {updateStatus && <TbLoaderQuarter className='text-xl animate-rotate  '/>}
             </button>
             :
 
