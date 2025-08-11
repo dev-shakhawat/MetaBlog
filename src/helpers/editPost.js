354a81213769d6f59f8b75af97cd14abc44224a4
shakhawat.dev <shakhawat.code@gmail.com>
2025-08-11 13:42:29 +0600

    update functuion solve

--- src/helpers/editPost.js
+++ src/helpers/editPost.js
@@ -5,8 +5,7 @@ import { hasStatus } from "../redux/slices/notificationSlice";
 
 
 async function editPost(postID , title , description , featuredImage , category , prevImage , setUpdateStatus , dispatch){
-
-  console.log(featuredImage);
+ 
   
     
     setUpdateStatus(true);
@@ -20,19 +19,15 @@ async function editPost(postID , title , description , featuredImage , category 
 
     const response = await axios.patch(`${import.meta.env.VITE_BASE_URL}/post/updatePost`, formData, { withCredentials: true, });
       
-      const data = response.data.data;
-
-      console.log(data);
-      
-      
+      const data = response.response.data;
       if(data){
         setUpdateStatus(false);
-        dispatch(loadpost());
         dispatch(hasStatus(data));
+        dispatch(editStatus({status: false , id: null}))
+        dispatch(loadpost());
         setTimeout(() => {
           dispatch(hasStatus(null));
         } , 2000);
-        dispatch(editStatus({status: false , id: null}))
       }
 }
 
