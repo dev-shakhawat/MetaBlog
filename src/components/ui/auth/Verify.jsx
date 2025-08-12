15914e0cebd1775c2177f90bc31a7870821439f4
shakhawat.dev <shakhawat.code@gmail.com>
2025-08-12 12:39:14 +0600

    account verification page added

--- src/components/ui/auth/Verify.jsx
+++ src/components/ui/auth/Verify.jsx
@@ -26,10 +26,10 @@ export default function Verify() {
     
 
   return (
-    <div className='md:w-1/2 w-3/4 mx-auto 2xl:mt-35 xl:mt-16 lg:mt-12 md:mt-10 sm:mt-8 mt-4   '>
+    <div className='md:w-1/2 w-3/4 mx-auto 2xl:pt-35 xl:pt-16 lg:pt-12 md:pt-10 sm:pt-8 pt-4   '>
         
         <h2 style={{color: color.textprimary}} className=" font-work-sans font-bold 2xl:text-4xl xl:text-3xl lg:text-2xl md:text-xl sm:text-lg text-base text-center    ">Account Verification</h2>
-        <p className=" font-work-sans text-xs md:text-sm lg:text-base text-center pb-5  ">Enter the OTP we send to your email</p>
+        <p style={{color: color.textsecondary}} className=" font-work-sans text-xs md:text-sm lg:text-base text-center pb-5  ">Enter the OTP we send to your email</p>
 
         <div className="mt-2">
 
