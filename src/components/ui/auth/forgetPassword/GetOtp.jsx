44b1bbe0dedd464bb1196d1611edccf22489d28f
shakhawat.dev <shakhawat.code@gmail.com>
2025-08-12 15:39:44 +0600

    reset password

--- src/components/ui/auth/forgetPassword/GetOtp.jsx
+++ src/components/ui/auth/forgetPassword/GetOtp.jsx
@@ -1,18 +1,25 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import InputField from '../../../common/InputField'
 import colorSchema from '../../../../colors/colorSchema';
 import axios from 'axios';
 import { hasStatus } from '../../../../redux/slices/notificationSlice';
 import { useDispatch } from 'react-redux';
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 export default function GetOtp({setIsotpdone}) {
 
     const color = colorSchema();
     const [email , setEmail] = useState('');
     const dispatch = useDispatch();
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+      window.scrollTo(0, 0);
+    }, [])
 
     const handleGetOtp = async () => { 
       const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      setIsLoading(true);
       try{
         if(emailRegex.test(email)){
            const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/auth/resetPassword`, {email});
@@ -20,6 +27,7 @@ export default function GetOtp({setIsotpdone}) {
            console.log(response);
            
            if(data.status){
+              setIsLoading(false);
               dispatch(hasStatus({status: true , message: data.sms}));
               setTimeout(() => {
                 dispatch(hasStatus(null));
@@ -27,12 +35,14 @@ export default function GetOtp({setIsotpdone}) {
            }
         }
         else{
+          setIsLoading(false);
           dispatch(hasStatus({status: false , message: 'Invalid email'}));
           setTimeout(() => {
             dispatch(hasStatus(null));
           }, 1500);
         }
       }catch(error){
+        setIsLoading(false);
         console.log(error);
       }
     }
@@ -45,7 +55,10 @@ export default function GetOtp({setIsotpdone}) {
         <div className="flex gap-2   ">
             <InputField  placeholder={`Your email`} className={`flex-1`} onChange={(e) => setEmail(e.target.value)}     />
 
-            <button onClick={handleGetOtp} type="submit" className=' h-inherit px-5 rounded-[6px] cursor-pointer  bg-blue-500 text-white font-work-sans font-medium text-xs sm:text-sm md:text-base leading-6   ' >Get OTP</button>
+            <button onClick={handleGetOtp} type="submit" className='flex items-center justify-center gap-2 h-inherit px-5 rounded-[6px] cursor-pointer  bg-blue-500 text-white font-work-sans font-medium text-xs sm:text-sm md:text-base leading-6   ' >
+              <span>Get OTP</span>
+              {isLoading && <AiOutlineLoading3Quarters className='animate-rotate' />}
+            </button>
 
         </div>
 
